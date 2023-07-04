4191de7303955ff669b94bcb8151a7c0e2b1975b
Krzysztof R <plkrozbicki@gmail.com>
2023-07-04 18:22:10 +0100

    Create vite template

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: 'https://krzysztofrozbicki.github.io/goit-react-hw-04-phonebook/',
+});
